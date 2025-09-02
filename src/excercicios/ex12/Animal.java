95717f26f2652400f078748d8b5f8ac777659f55
Rystei <gustavo.telles@edu.unifil.br>
2025-09-02 09:40:22 -0300

    Exercicio de fixação herança

---
+++ src/excercicios/ex12/Animal.java
@@ -0,0 +1,7 @@
+package excercicios.ex12;
+
+public class Animal {
+    public void emitirSom() {
+        System.out.println("Som do animal");
+    }
+}
---
+++ src/excercicios/ex12/MainEx12.java
@@ -0,0 +1,13 @@
+package excercicios.ex12;
+
+public class MainEx12 {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        cachorro.emitirSom();
+        cachorro.abanarRabo();
+
+        Gato gato = new Gato();
+        gato.emitirSom();
+        gato.arranharMoveis();
+    }
+}
