65eb69b1ed5adb01dfed16bb16dc92d485347580
Rystei <gustavo.telles@edu.unifil.br>
2025-09-02 11:19:36 -0300

    Exercicios de fixação Interface

---
+++ src/excercicios/ex16/CalculoGeometrico.java
@@ -0,0 +1,6 @@
+package excercicios.ex16;
+
+public interface CalculoGeometrico {
+    void calcularArea(double altura, double largura);
+    void calcularPerimetro(double altura, double largura);
+}
---
+++ src/excercicios/ex16/MainEx16.java
@@ -0,0 +1,9 @@
+package excercicios.ex16;
+
+public class MainEx16 {
+    public static void main(String[] args) {
+        CalculadoraSalaRetangular calculadora = new CalculadoraSalaRetangular();
+        calculadora.calcularArea(5, 8);
+        calculadora.calcularPerimetro(5,8);
+    }
+}
